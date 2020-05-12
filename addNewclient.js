13c53f40905dfabb5443f71c1100782f447d88c3
João Pedro Purcinelli <53058735+jowpurcinelli@users.noreply.github.com>
2020-05-12 19:08:46 -0300

    add the NewClient JS file
    The file consists in a function that inserts the new client into the future back end, which is in the creation process, to store them.

---
+++ addNewclient.js
@@ -0,0 +1,73 @@
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-client-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+const addButton = document.querySelector('[button-add-to-list]')
+
+// const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+
+
+// let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+// let lists = [{
+    
+//     name: 'Maria',
+//     adress: 'la puta madre'
+
+// }, {
+    
+//     name: 'Jose',
+//     adress: 'la puta q te pario'
+// }]
+
+
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    // lists.push(list)
+    saveAndRender()
+    
+})
+
+function createList(name){
+    return { id: Date.now().toString(), name: name, adress: [] }
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+// function save() {
+//     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+// }
+
+
+ 
+
+
+
+    
+
+
+function render() {
+    
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add('list-name')
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement)        
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
